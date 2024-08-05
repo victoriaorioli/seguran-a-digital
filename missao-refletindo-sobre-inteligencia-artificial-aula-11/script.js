c8f2d7fdedd726ada4680e8f643374b197cee584
victoriaorioli <106542162+victoriaorioli@users.noreply.github.com>
2024-08-05 09:02:31 -0300

    Update script.js

--- missao-refletindo-sobre-inteligencia-artificial-aula-11/script.js
+++ missao-refletindo-sobre-inteligencia-artificial-aula-11/script.js
@@ -32,11 +32,11 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "O sistema de atendimento funciona de modo descentralizado e hierarquizado.",
         alternativas: [
             {
                 texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                afirmacao: "Outro problema de saúde que que deve ser analisado com cautela é a obesidade, que cresce a níveis alarmantes"
             },
             {
                 texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
